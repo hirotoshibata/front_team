2e712b14dcaf4a91491291f86292bdc511072654
huru_26 <w0rk.we6+sns@gmail.com>
2019-07-07 22:35:50 +0800

    （修正）View : html ＆ js  グラフ競合の回避

--- now-ui-dashboard-master/assets/demo/demo.js
+++ now-ui-dashboard-master/assets/demo/demo.js
@@ -2,185 +2,226 @@
 *    main チャート
 ***********************************************************/
 
-demo = {
-  //Month
-  initDashboardPageChartsMonth: function()
-  {
-    chartColor = "#FFFFFF";
-
-    let ctx = document.getElementById('bigDashboardChart').getContext("2d");
-    let gradientFill = ctx.createLinearGradient(0, 200, 0, 50);
-    gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
-    gradientFill.addColorStop(1, "rgba(255, 255, 255, 0.24)");
-
-    let myChart = new Chart(ctx, {
-      type    : 'line',
-      data    : {
-        labels: ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
-        datasets: [{
-          label: "Data",
-          borderColor: chartColor,
-          pointBorderColor: chartColor,
-          pointBackgroundColor: "#1e3d60",
-          pointHoverBackgroundColor: "#1e3d60",
-          pointHoverBorderColor: chartColor,
-          pointBorderWidth: 1,
-          pointHoverRadius: 7,
-          pointHoverBorderWidth: 2,
-          pointRadius: 5,
-          fill: true,
-          backgroundColor: gradientFill,
-          borderWidth: 2,
-          data: [50, 150, 100, 190, 130, 190, 250, 160, 120, 140, 190, 95]
-        }]
-      },
-      options : {
-        layout: {
-          padding: {
-            left: 20,
-            right: 20,
-            top: 0,
-            bottom: 0
-          }
-        },
-        maintainAspectRatio: false,
-        tooltips: {
-          backgroundColor: '#fff',
-          titleFontColor: '#333',
-          bodyFontColor: '#666',
-          bodySpacing: 4,
-          xPadding: 12,
-          mode: "nearest",
-          intersect: 0,
-          position: "nearest"
-        },
-        legend: {
-          position: "bottom",
-          fillStyle: "#FFF",
-          display: false
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              fontColor: "rgba(255,255,255,0.4)",
-              fontStyle: "bold",
-              beginAtZero: true,
-              maxTicksLimit: 5,
-              padding: 10
-            },
-            gridLines: {
-              drawTicks: true,
-              drawBorder: false,
-              display: true,
-              color: "rgba(255,255,255,0.1)",
-              zeroLineColor: "transparent"
-            }
-
-          }],
-          xAxes: [{
-            gridLines: {
-              zeroLineColor: "transparent",
-              display: false,
-
-            },
-            ticks: {
-              padding: 10,
-              fontColor: "rgba(255,255,255,0.4)",
-              fontStyle: "bold"
-            }
-          }]
+// ページ読み込み時
+// demo = {
+//   initDashboardPageChartsWeek: function()
+//   {
+//     myChart = new Chart(ctx, {
+//       type    : 'line',
+//       data    : {
+//         labels: ["MON", "TUE", "WEN", "THE", "FRI", "SAT", "SUN"],
+//         datasets: [{
+//           label: "Data",
+//           borderColor: chartColor,
+//           pointBorderColor: chartColor,
+//           pointBackgroundColor: "#1e3d60",
+//           pointHoverBackgroundColor: "#1e3d60",
+//           pointHoverBorderColor: chartColor,
+//           pointBorderWidth: 1,
+//           pointHoverRadius: 7,
+//           pointHoverBorderWidth: 2,
+//           pointRadius: 5,
+//           fill: true,
+//           backgroundColor: gradientFill,
+//           borderWidth: 2,
+//           data: [250, 160, 120, 140, 190, 95, 78]
+//         }]
+//       },
+//       options : {
+//         layout: {
+//           padding: {
+//             left: 20,
+//             right: 20,
+//             top: 0,
+//             bottom: 0
+//           }
+//         },
+//         maintainAspectRatio: false,
+//         tooltips: {
+//           backgroundColor: '#fff',
+//           titleFontColor: '#333',
+//           bodyFontColor: '#666',
+//           bodySpacing: 4,
+//           xPadding: 12,
+//           mode: "nearest",
+//           intersect: 0,
+//           position: "nearest"
+//         },
+//         legend: {
+//           position: "bottom",
+//           fillStyle: "#FFF",
+//           display: false
+//         },
+//         scales: {
+//           yAxes: [{
+//             ticks: {
+//               fontColor: "rgba(255,255,255,0.4)",
+//               fontStyle: "bold",
+//               beginAtZero: true,
+//               maxTicksLimit: 5,
+//               padding: 10
+//             },
+//             gridLines: {
+//               drawTicks: true,
+//               drawBorder: false,
+//               display: true,
+//               color: "rgba(255,255,255,0.1)",
+//               zeroLineColor: "transparent"
+//             }
+//
+//           }],
+//           xAxes: [{
+//             gridLines: {
+//               zeroLineColor: "transparent",
+//               display: false,
+//
+//             },
+//             ticks: {
+//               padding: 10,
+//               fontColor: "rgba(255,255,255,0.4)",
+//               fontStyle: "bold"
+//             }
+//           }]
+//         }
+//       }
+//     });
+//   },
+// };
+function initDashboardPageChartsWeek(){
+  myChart = new Chart(ctx, {
+    type    : 'line',
+    data    : {
+      labels: ["MON", "TUE", "WEN", "THE", "FRI", "SAT", "SUN"],
+      datasets: [{
+        label: "Data",
+        borderColor: chartColor,
+        pointBorderColor: chartColor,
+        pointBackgroundColor: "#1e3d60",
+        pointHoverBackgroundColor: "#1e3d60",
+        pointHoverBorderColor: chartColor,
+        pointBorderWidth: 1,
+        pointHoverRadius: 7,
+        pointHoverBorderWidth: 2,
+        pointRadius: 5,
+        fill: true,
+        backgroundColor: gradientFill,
+        borderWidth: 2,
+        data: [250, 160, 120, 140, 190, 95, 78]
+      }]
+    },
+    options : {
+      layout: {
+        padding: {
+          left: 20,
+          right: 20,
+          top: 0,
+          bottom: 0
         }
-      }
-    });
-  },
-  // Week
-  initDashboardPageChartsWeek: function()
-  {
-    chartColor = "#FFFFFF";
-
-    let ctx = document.getElementById('bigDashboardChart').getContext("2d");
-    let gradientFill = ctx.createLinearGradient(0, 200, 0, 50);
-    gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
-    gradientFill.addColorStop(1, "rgba(255, 255, 255, 0.24)");
-
-    let myChart = new Chart(ctx, {
-      type    : 'line',
-      data    : {
-        labels: ["MON", "TUE", "WEN", "THE", "FRI", "SAT", "SUN"],
-        datasets: [{
-          label: "Data",
-          borderColor: chartColor,
-          pointBorderColor: chartColor,
-          pointBackgroundColor: "#1e3d60",
-          pointHoverBackgroundColor: "#1e3d60",
-          pointHoverBorderColor: chartColor,
-          pointBorderWidth: 1,
-          pointHoverRadius: 7,
-          pointHoverBorderWidth: 2,
-          pointRadius: 5,
-          fill: true,
-          backgroundColor: gradientFill,
-          borderWidth: 2,
-          data: [250, 160, 120, 140, 190, 95,78]
-        }]
       },
-      options : {
-        layout: {
-          padding: {
-            left: 20,
-            right: 20,
-            top: 0,
-            bottom: 0
+      maintainAspectRatio: false,
+      tooltips: {
+        backgroundColor: '#fff',
+        titleFontColor: '#333',
+        bodyFontColor: '#666',
+        bodySpacing: 4,
+        xPadding: 12,
+        mode: "nearest",
+        intersect: 0,
+        position: "nearest"
+      },
+      legend: {
+        position: "bottom",
+        fillStyle: "#FFF",
+        display: false
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: "rgba(255,255,255,0.4)",
+            fontStyle: "bold",
+            beginAtZero: true,
+            maxTicksLimit: 5,
+            padding: 10
+          },
+          gridLines: {
+            drawTicks: true,
+            drawBorder: false,
+            display: true,
+            color: "rgba(255,255,255,0.1)",
+            zeroLineColor: "transparent"
           }
-        },
-        maintainAspectRatio: false,
-        tooltips: {
-          backgroundColor: '#fff',
-          titleFontColor: '#333',
-          bodyFontColor: '#666',
-          bodySpacing: 4,
-          xPadding: 12,
-          mode: "nearest",
-          intersect: 0,
-          position: "nearest"
-        },
-        legend: {
-          position: "bottom",
-          fillStyle: "#FFF",
-          display: false
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              fontColor: "rgba(255,255,255,0.4)",
-              fontStyle: "bold",
-              beginAtZero: true,
-              maxTicksLimit: 5,
-              padding: 10
-            },
-            gridLines: {
-              drawTicks: true,
-              drawBorder: false,
-              display: true,
-              color: "rgba(255,255,255,0.1)",
-              zeroLineColor: "transparent"
-            }
 
-          }],
-          xAxes: [{
-            gridLines: {
-              zeroLineColor: "transparent",
-              display: false,
+        }],
+        xAxes: [{
+          gridLines: {
+            zeroLineColor: "transparent",
+            display: false,
 
-            },
-            ticks: {
-              padding: 10,
-              fontColor: "rgba(255,255,255,0.4)",
-              fontStyle: "bold"
-            }
-          }]
-        }
+          },
+          ticks: {
+            padding: 10,
+            fontColor: "rgba(255,255,255,0.4)",
+            fontStyle: "bold"
+          }
+        }]
       }
-    });
-  },
-};
+    }
+  });
+}
+
+//Week
+function chnageWeekChart(chart) {
+  chart.data = {
+    labels: ["MON", "TUE", "WEN", "THE", "FRI", "SAT", "SUN"],
+    datasets: [{
+      label: "Data",
+      borderColor: chartColor,
+      pointBorderColor: chartColor,
+      pointBackgroundColor: "#1e3d60",
+      pointHoverBackgroundColor: "#1e3d60",
+      pointHoverBorderColor: chartColor,
+      pointBorderWidth: 1,
+      pointHoverRadius: 7,
+      pointHoverBorderWidth: 2,
+      pointRadius: 5,
+      fill: true,
+      backgroundColor: gradientFill,
+      borderWidth: 2,
+      data: [250, 160, 120, 140, 190, 95, 78]
+    }]
+  }
+  chart.update();
+}
+
+//Month
+function changeMonthChart(chart) {
+  chart.data = {
+    labels: ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
+    datasets: [{
+      label: "Data",
+      borderColor: chartColor,
+      pointBorderColor: chartColor,
+      pointBackgroundColor: "#1e3d60",
+      pointHoverBackgroundColor: "#1e3d60",
+      pointHoverBorderColor: chartColor,
+      pointBorderWidth: 1,
+      pointHoverRadius: 7,
+      pointHoverBorderWidth: 2,
+      pointRadius: 5,
+      fill: true,
+      backgroundColor: gradientFill,
+      borderWidth: 2,
+      data: [50, 150, 100, 190, 130, 190, 250, 160, 120, 140, 190, 95]
+    }]
+  }
+  chart.update();
+}
+
+
+chartColor = "#FFFFFF";
+
+let ctx = document.getElementById('bigDashboardChart').getContext("2d");
+let gradientFill = ctx.createLinearGradient(0, 200, 0, 50);
+gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
+gradientFill.addColorStop(1, "rgba(255, 255, 255, 0.24)");
+let myChart = new Chart(ctx, {});
